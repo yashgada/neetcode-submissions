abadefa0fbc2eb98d85ca622a7ebae1eef5945d0
yashgada <gadayash9@gmail.com>
2026-08-05 17:14:13 +0530

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const obj: { [key: number]: number } = {};
+        for (let n of nums) {
+            obj[n] = (obj[n] || 0) + 1;
+        }
+        // console.log(obj)
+        return Object.entries(obj)
+            .sort((a: any, b: any) => b[1] - a[1])
+            .slice(0,k)
+            .map((a) => +a[0]);
+    }
+}
